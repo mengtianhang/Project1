063338eab1baeafd3a894a7900895665f29f5108
LAICHAO20188 <619642846@qq.com>
2020-09-08 20:04:54 +0800

    1

--- laichao/111.cpp
+++ laichao/111.cpp
@@ -7,16 +7,8 @@ using namespace std;
 int main() {
 	string str;
 	while (cin >> str) {
-		stack<char>sSta;
-		for (int i = 0; i < str.size(); i++) {
-			sSta.push(str[i]);
-		}
-		string res;
-		while (!sSta.empty()) {
-			res += sSta.top();
-			sSta.pop();
-		}
-		cout << res << endl;
+		reverse(str.begin(), str.end());
+		cout << str << endl;
 	}
 	return 0;
 }
