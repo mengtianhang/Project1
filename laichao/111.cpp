e655dd0f80b564cccedbcf634b99842235e587cb
LAICHAO20188 <619642846@qq.com>
2020-09-08 20:24:33 +0800

    1111

--- laichao/111.cpp
+++ laichao/111.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 #include <string>
 using namespace std;
+
 int main() {
-	int a, int b;
-	cin >> a >> b;
+	stack<char>sSta;
+	char tmp;
+	while (tmp = getchar()) {
+		sSta.push(tmp);
+	}
+	string res;
+	while (!sSta.empty()) {
+		res += sSta.top;
+		sSta.pop();
+	}
+	cout << res << endl;
 
-
-	system("pause");
+	//system("pause");
 	return 0;
 }
