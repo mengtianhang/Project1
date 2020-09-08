1efb30a043e7915f58b4ba50980e13def8d6fd56
LAICHAO20188 <619642846@qq.com>
2020-09-08 20:30:23 +0800

    11

--- laichao/111.cpp
+++ laichao/111.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 #include <string>
 using namespace std;
 int main() {
 	string s;
 	while (getline(cin, s)) {
-		reverse(s.begin(), s.end());
+		//reverse(s.begin(), s.end());
+		stack<char>ss;
+		for (int i = 0; i < s.size(); i++) {
+			ss.push(s[i]);
+		}
+		string res;
+		while (!ss.empty()) {
+			res += ss.top();
+			ss.pop();
+		}
 		cout << s << endl;
 	}
 
