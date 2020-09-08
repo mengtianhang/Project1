7d533ffdce41d47b309f39f42478131a477bac86
LAICHAO20188 <619642846@qq.com>
2020-09-08 20:17:27 +0800

    111

--- laichao/源.c
+++ laichao/源.c
@@ -1,4 +1,6 @@
-#include "stdio.h"
+//#include "stdio.h"
+#include <iostream>
+using namespace std;
 
 void DecToN(long num, int B) {
 	int a[100] = { 0 };
@@ -7,25 +9,29 @@ void DecToN(long num, int B) {
 		a[count++] = num%B;  //保存余数  
 		num = num / B;
 	}
-	for (int i = count - 1; i >= 0; i--){
+	for (int i = count - 1; i >= 0; i--) {
 		if (a[i] >= 10)//十六进制要特殊处理  
 		{
-			printf("%c", 'A' + a[i] - 10);
+			//printf("%c", 'A' + a[i] - 10);
+			char c = 'A' + a[i] - 10;
+			cout << c;
 		}
 		else
 		{
-			printf("%d", a[i]);
+			//printf("%d", a[i]);
+			cout << a[i] << endl;
 		}
 	}
-	printf("\n");
+	//printf("\n");
+	cout << endl;
 }
 
 int main(int argc, char* argv[])
 {
 	long num;
 	int B;
-	//cin >> B >> num;
-	sscanf("%d,%d", &B, &num);
+	cin >> B >> num;
+	//sscanf("%d,%d", &B, &num);
 	DecToN(num, B);
 
 	return 0;
